bdfb1e73cff67ff3ff210f5e9cda91ff5b517f20
Ринат <adievrinat@yandex.ru>
2020-05-27 11:35:41 +0700

    scrollAnimation.js - изменения в условии

--- src/scripts/utils/scrollAnimation.js
+++ src/scripts/utils/scrollAnimation.js
@@ -1,12 +1,13 @@
 const scrollAnimation = (block, cls, offset = 0, infinity = false) => {
   let position = block.getBoundingClientRect();
-  let top = position.top + window.pageYOffset ;
+  let top = position.top + window.pageYOffset;
   let hasClass = block.classList.contains('scroll-animation');
+  let hasMod = block.classList.contains(cls);
 
   if (typeof block === 'object' && block && typeof cls === 'string') {
-    if (window.pageYOffset  + window.innerHeight + offset >= top && !block.classList.contains(cls) && hasClass) {
+    if (window.pageYOffset + window.innerHeight + offset >= top && !hasMod && hasClass) {
       block.classList.add(cls);
-    } else if (window.pageYOffset  + window.innerHeight + offset < top && block.classList.contains(cls) && hasClass && infinity) {
+    } else if (window.pageYOffset + window.innerHeight + offset < top && hasMod && hasClass && infinity) {
       block.classList.remove(cls);
     }
   }
